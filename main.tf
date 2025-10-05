3b9001657d1b517ef7207226a8cc4bba2d30eee0
Balamurali Ramakrishnan <balamurali.ramakrishnan@infoservices.com>
2025-10-05 20:38:41 +0530

    AZmanageid

--- main.tf
+++ main.tf
@@ -123,13 +123,15 @@ resource "azurerm_linux_virtual_machine" "vm" {
   admin_username        = var.admin_username
   network_interface_ids = [azurerm_network_interface.nic.id]
 
-  dynamic admin_ssh_key {
-    for_each = var.ssh_public_key != null ? ["fake"] : []
-    content {
-    username   = var.admin_username
-    public_key = data.azurerm_ssh_public_key.SshPublicKey.public_key
+  dynamic "admin_ssh_key" {
+  for_each = length(var.ssh_public_key) > 0 ? toset([var.ssh_public_key]) : []
+
+  content {
+    username   = "azureuser"
+    public_key = admin_ssh_key.value
   }
-  }
+}
+
 
   source_image_reference {
     publisher = "Canonical"
